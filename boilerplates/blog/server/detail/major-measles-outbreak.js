8de393d88388efd9f5b3bb81e71c9c5000d4aa6b
Christian Blanquera <cblanquera@openovate.com>
2015-02-09 08:07:22 +0800

    moving again

--- boilerplates/blog/server/detail/major-measles-outbreak.js
+++ boilerplates/blog/server/detail/major-measles-outbreak.js
@@ -1,17 +1,17 @@
 module.exports = {
 	"error": false,
 	"results": {
-		"slug": "exodus-from-egypt",
-		"title": "Exodus from Egypt: The country's youth move on",
-		"image": "/boilerplates/blog/images/dependent-100343_640.jpg",
+		"slug": "major-measles-outbreak",
+		"title": "Major measles outbreak linked to Disneyland infects 78",
+		"image": "/mobility/boilerplates/blog/images/girl-102831_640.jpg",
 		"category": "world",
-		"shares": "753",
+		"shares": "1145",
 		"detail": "<p>Maecenas sed diam eget risus varius blandit sit amet non magna. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p> <p>Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p> <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla.</p> <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>",
 		"related": [
 			{
 				"slug": "a-knife-in-an-enchilada",
 				"title": "A knife in an enchilada: The oddest thing the TSA seized in 2014",
-				"image": "/boilerplates/blog/images/american-football-63109_640.jpg",
+				"image": "/mobility/boilerplates/blog/images/american-football-63109_640.jpg",
 				"category": "watercooler",
 				"published": "3 days ago",
 				"shares": "814"
@@ -19,7 +19,7 @@ module.exports = {
 			{
 				"slug": "leatherman-to-debut",
 				"title": "Leatherman to debut a wearable multi-tool this summer",
-				"image": "/boilerplates/blog/images/lacrosse-165576_640.jpg",
+				"image": "/mobility/boilerplates/blog/images/lacrosse-165576_640.jpg",
 				"category": "lifestyle",
 				"published": "2 days ago",
 				"shares": "1235"
@@ -27,7 +27,7 @@ module.exports = {
 			{
 				"slug": "elon-musk-is-peak",
 				"title": "Elon Musk is peak Elon Musk in 'Simpsons' sneak peek",
-				"image": "/boilerplates/blog/images/mountain-91385_640.jpg",
+				"image": "/mobility/boilerplates/blog/images/mountain-91385_640.jpg",
 				"category": "entertainment",
 				"published": "1 hour ago",
 				"shares": "1335"
@@ -37,19 +37,19 @@ module.exports = {
 			{
 				"author": "Stacy",
 				"published": "2 days ago",
-				"avatar": "/boilerplates/blog/images/avatar-1.jpg",
+				"avatar": "/mobility/boilerplates/blog/images/avatar-1.jpg",
 				"detail": "<p>This is great news! Sharing to my friends</p>"
 			},
 			{
 				"author": "Eva For",
 				"published": "3 days ago",
-				"avatar": "/boilerplates/blog/images/avatar-2.jpg",
+				"avatar": "/mobility/boilerplates/blog/images/avatar-2.jpg",
 				"detail": "<p>Love this article. Keep up the good work. I saw this somewhere else, but this one is a better explanation.</p>"
 			},
 			{
 				"author": "George Kuman",
 				"published": "2 days ago",
-				"avatar": "/boilerplates/blog/images/avatar-3.jpg",
+				"avatar": "/mobility/boilerplates/blog/images/avatar-3.jpg",
 				"detail": "<p>I'm a little bit dissapointed, but hey i guess this is how the world works. Fine article none the less.</p>"
 			}
 		]
--- boilerplates/blog/server/rising.js
+++ boilerplates/blog/server/rising.js
@@ -2,60 +2,60 @@ module.exports = {
 	"error": false,
 	"results": [
 		{
-			"slug": "budi-the-orangutan",
-			"title": "Budi the orangutan is recovering after almost a year of neglect",
-			"image": "/boilerplates/blog/images/africa-139343_640.jpg",
-			"category": "watercooler",
-			"shares": "2845"
-		},
-		{
-			"slug": "a-knife-in-an-enchilada",
-			"title": "A knife in an enchilada: The oddest thing the TSA seized in 2014",
-			"image": "/boilerplates/blog/images/american-football-63109_640.jpg",
-			"category": "watercooler",
-			"shares": "814"
-		},
-		{
-			"slug": "nhl-teams-up-with-gopro",
-			"title": "NHL teams up with GoPro to broadcast games from new angles",
-			"image": "/boilerplates/blog/images/couple-168191_640.jpg",
-			"category": "entertainment",
-			"shares": "924"
-		},
-		{
-			"slug": "young-women-are-the-heroes",
-			"title": "Young women are the heroes of campus rape doc 'The Huntting Ground'",
-			"image": "/boilerplates/blog/images/daisy-75190_640.jpg",
-			"category": "entertainment",
-			"shares": "793"
-		},
-		{
-			"slug": "exodus-from-egypt",
-			"title": "Exodus from Egypt: The country's youth move on",
-			"image": "/boilerplates/blog/images/dependent-100343_640.jpg",
-			"category": "world",
-			"shares": "753"
-		},
-		{
-			"slug": "major-measles-outbreak",
-			"title": "Major measles outbreak linked to Disneyland infects 78",
-			"image": "/boilerplates/blog/images/girl-102831_640.jpg",
-			"category": "world",
-			"shares": "1145"
-		},
-		{
 			"slug": "leatherman-to-debut",
 			"title": "Leatherman to debut a wearable multi-tool this summer",
-			"image": "/boilerplates/blog/images/lacrosse-165576_640.jpg",
+			"image": "/mobility/boilerplates/blog/images/lacrosse-165576_640.jpg",
 			"category": "lifestyle",
 			"shares": "1235"
 		},
 		{
 			"slug": "elon-musk-is-peak",
 			"title": "Elon Musk is peak Elon Musk in 'Simpsons' sneak peek",
-			"image": "/boilerplates/blog/images/mountain-91385_640.jpg",
+			"image": "/mobility/boilerplates/blog/images/mountain-91385_640.jpg",
 			"category": "entertainment",
 			"shares": "1335"
+		},
+		{
+			"slug": "budi-the-orangutan",
+			"title": "Budi the orangutan is recovering after almost a year of neglect",
+			"image": "/mobility/boilerplates/blog/images/africa-139343_640.jpg",
+			"category": "watercooler",
+			"shares": "2845"
+		},
+		{
+			"slug": "a-knife-in-an-enchilada",
+			"title": "A knife in an enchilada: The oddest thing the TSA seized in 2014",
+			"image": "/mobility/boilerplates/blog/images/american-football-63109_640.jpg",
+			"category": "watercooler",
+			"shares": "814"
+		},
+		{
+			"slug": "nhl-teams-up-with-gopro",
+			"title": "NHL teams up with GoPro to broadcast games from new angles",
+			"image": "/mobility/boilerplates/blog/images/couple-168191_640.jpg",
+			"category": "entertainment",
+			"shares": "924"
+		},
+		{
+			"slug": "young-women-are-the-heroes",
+			"title": "Young women are the heroes of campus rape doc 'The Huntting Ground'",
+			"image": "/mobility/boilerplates/blog/images/daisy-75190_640.jpg",
+			"category": "entertainment",
+			"shares": "793"
+		},
+		{
+			"slug": "exodus-from-egypt",
+			"title": "Exodus from Egypt: The country's youth move on",
+			"image": "/mobility/boilerplates/blog/images/dependent-100343_640.jpg",
+			"category": "world",
+			"shares": "753"
+		},
+		{
+			"slug": "major-measles-outbreak",
+			"title": "Major measles outbreak linked to Disneyland infects 78",
+			"image": "/mobility/boilerplates/blog/images/girl-102831_640.jpg",
+			"category": "world",
+			"shares": "1145"
 		}
 	]
 }
